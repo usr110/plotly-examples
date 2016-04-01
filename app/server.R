f4df69b78a808d38b3a0e057d75a4b986d2fdf89
usr110 <m.aliabbas@gmail.com>
2016-04-01 11:30:31 +0100

    Change code to plot to an temporary file

--- app/server.R
+++ app/server.R
@@ -7,10 +7,21 @@ shinyServer(function(input, output) {
     # size of the bins depend on the input 'bins'
     size <- (maxx - minx) / input$bins
     
+#     gg <- ggplot(movies, aes(x=rating)) +
+#       geom_histogram(aes(y = ..density..), binwidth=size)
+#     # Convert the ggplot to a plotly
+#     p <- ggplotly(gg)
+#     p
+    
+    
+    outfile <- tempfile(fileext='.png')
+    
+    # Generate the PNG
+    png(outfile, width=400, height=300)
     gg <- ggplot(movies, aes(x=rating)) +
       geom_histogram(aes(y = ..density..), binwidth=size)
-    # Convert the ggplot to a plotly
     p <- ggplotly(gg)
+    dev.off()
     p
   })
 })
